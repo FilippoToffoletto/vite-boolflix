07576815be9b740efc12d022c8e04be440409bcf
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-21 17:26:29 +0100

    milestone2

--- src/components/DynamicCards.vue
+++ src/components/DynamicCards.vue
@@ -6,6 +6,28 @@ export default {
         titoloOriginale: String,
         lingua: String,
         voto:Number
+    },
+    data(){
+        return{
+            languageNotExist: ''
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+        getClassName(){
+            if(this.lingua === 'en'){
+                return 'fi fi-' + 'gb';
+            }else{
+                return 'fi fi-' + this.lingua
+            }
+        },
+        languageNotFound(){
+            if(this.languageNotExist.includes(this.lingua)){
+                return 'NOT FOUND'
+            }
+        }
     }
 }
 </script>
@@ -14,8 +36,11 @@ export default {
   <li>
     <h3>titolo: {{titolo}}</h3>
     <h3>titolo originale:  {{titoloOriginale}}</h3>
-    <h3>lingua: 
-        <span :class="getClassName">{{languageNotExist}}</span>
+    <h3>
+        lingua: 
+        <span :class="getClassName">
+            {{languageNotExist}}
+        </span>
     </h3>
     <h3>voto: {{voto / 2}}</h3>
   </li>
